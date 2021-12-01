8d700f66da39e037a448de00b93147e3fb79b72b
Raϕ <72984363+ARyOtaRe@users.noreply.github.com>
2021-12-01 15:55:49 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
+import pprint
+
+
+pp = pprint.PrettyPrinter(indent=4)
+
 
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
-  'start':'1',
-  'limit':'5000',
+  'start':'26',
+  'limit':'1',
   'convert':'USD'
 }
+
+
 headers = {
   'Accepts': 'application/json',
   'X-CMC_PRO_API_KEY': 'a80e718a-61fa-4358-80c0-14b3cbcae1bd',
@@ -18,21 +25,20 @@ session.headers.update(headers)
 
 try:
     response = session.get(url, params=parameters)
+  
+
     oeuf = json.loads(response.text)
-    #with open('test.json', 'w') as file:
-    #    caca=json.dumps(data, indent=2)
-    #    file.write(caca)
-    #print(data)
+    with open('tests.json', 'w') as file:
+      caca=json.dumps(oeuf, indent=2)
+      file.write(caca)
+#      pp.pprint(oeuf)
+      
     
-    print(oeuf["data"][2]["id"])
+ #   print(oeuf["data"][2]["id"])
 except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
+  pp.pprint(e)
 
 
-with open('nul.json', 'r') as file:
-    #caca=json.dumps(data, indent=2)
-    #file.write(caca)
-    #print(data)
-    oeuf = json.loads(file.read())
-    
-    print(oeuf["data"][22]["quote"]["USD"]["price"])
+with open('tests.json', 'r') as file:
+    oof = json.loads(file.read())
+    pp.pprint(oof["data"][0]["name"])
